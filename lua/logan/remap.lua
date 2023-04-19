0e5f5a71af6460c27aad1400578a826cac348849
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2023-04-19 14:58:01 -0700

    Organize

--- lua/logan/remap.lua
+++ lua/logan/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext)
-
 vim.keymap.set("n", "<leader>tp", vim.cmd.tabprevious)
+
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext)
 
 vim.keymap.set("n", "<leader>tN", vim.cmd.tabnew)
