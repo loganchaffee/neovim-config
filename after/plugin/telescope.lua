572cba100236e7e3440f2f31a942a510b04cfeec
Logan Chaffee <loganschaffee@gmail.com>
2023-04-22 15:58:18 -0700

    Launch telescop live_grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -14,14 +14,12 @@ require('telescope').setup{
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
-vim.keymap.set('n', '<leader>ps', function() 
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+-- vim.keymap.set('n', '<leader>ps', function() 
+--     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
+vim.api.nvim_set_keymap('n', "<leader>ps", ":Telescope live_grep \n", { noremap = true })
 
-vim.api.nvim_set_keymap("n", "<space>pb", ":Telescope file_browser \n", {
-    noremap = true
-})
-
+vim.api.nvim_set_keymap("n", "<space>pb", ":Telescope file_browser \n", { noremap = true })
 
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
